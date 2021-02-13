8c398c6a441bf0be9cc13dd1521d4410d633a90a
minenash <minenash@protonmail.com>
2021-02-12 21:04:07 -0500

    Setup project

---
+++ src/main/java/com/minenash/action_hunger/ActionHunger.java
@@ -0,0 +1,8 @@
+package com.minenash.action_hunger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ActionHunger implements ModInitializer {
+	@Override
+	public void onInitialize() { }
+}
